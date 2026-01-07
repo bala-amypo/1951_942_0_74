7691e7f628bd887fe52e2db2402fe941e5f8a83a
bala-amypo <bala@amypo.in>
2026-01-07 06:09:53 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,12 +1,9 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.*;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -15,25 +12,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Transport Route Optimization API")
-                        .description("Backend APIs for Transport Route Optimization System")
-                        .version("1.0")
-                )
-                // In-built platform URL (DO NOT CHANGE)
+                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9252.408procr.amypo.ai/")
-                ))
-                // JWT support in Swagger
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .name("Authorization")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                );
-    }
+                        new Server().url("https://9243.408procr.amypo.ai/")
+                ));
+        }
 }
